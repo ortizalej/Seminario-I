b7f0dbe98e18dbb7709f7a147fae8bbb12ea5b2a
Alejandro <ortizalej199@gmail.com>
2021-05-01 19:37:38 -0300

    init src

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
